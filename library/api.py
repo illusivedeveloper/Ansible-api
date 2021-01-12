b23e6d416dc05788c676ecf02142a0388a916b2b
Nitish <illusivedeveloper@gmail.com>
2021-01-12 19:48:20 +0530

    PUT, PATCH, DELETE

--- library/api.py
+++ library/api.py
@@ -12,8 +12,31 @@ def getcall(url):
 
 def postcall(url, data):
     r = requests.post(url=url, data=data)
-    module.exit_json(changed=False, response=r.json())
+    if r.status_code == 201:
+        module.exit_json(changed=False, response=r.json())
+    else:
+        module.fail_json(msg="Error calling api.")
 
+def putcall(url, data):
+    r = requests.put(url=url, data=data)
+    if r.status_code == 200 or 204 or 201:
+        module.exit_json(changed=False, response=r.json())
+    else:
+        module.fail_json(msg="Error calling api.")
+
+def patchcall(url, data):
+    r = requests.patch(url=url, data=data)
+    if r.status_code == 200 or 204:
+        module.exit_json(changed=False, response=r.json())
+    else:
+        module.fail_json(msg="Error calling api.")
+
+def deletecall(url):
+    r = requests.delete(url)
+    if r.status_code == 200 or 202 or 204:
+        module.exit_json(changed=False, response=r.json())
+    else:
+        module.fail_json(msg="Error calling api.")
 
 def main():
     global module
@@ -33,6 +56,20 @@ def main():
         url = module.params['url']
         data = json.dumps(module.params['data'])
         postcall(url, data)
+
+    if module.params['action'] == 'PUT':
+        url = module.params['url']
+        data = json.dumps(module.params['data'])
+        putcall(url, data)
+
+    if module.params['action'] == 'PATCH':
+        url = module.params['url']
+        data = json.dumps(module.params['data'])
+        patchcall(url, data)
+
+    if module.params['action'] == 'DELETE':
+        url = module.params['url']
+        deletecall(url)
 
 if __name__ == '__main__':
     main()
